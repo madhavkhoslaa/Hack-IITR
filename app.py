fa364f2dc9b2d761a43d5a786dbb54729203daac
Madhav Khosla <madhav.khoslaa@gmail.com>
2020-02-02 00:17:49 +0530

    Added help at /

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ import pandas as pd
 import numpy as np
 import json
 import random
+import psycopg2
+
 
 app = Flask(__name__)
+
+TOKEN_LIST=['DWUDBWQFIVEFESZJNFEFNZEFN']
 
 def argparser_(strr, delim_='%'):
     return list(map(int, strr.split(delim_)))
@@ -46,9 +50,27 @@ def what2grow(arr):
     preds = Label_Crops_type.inverse_transform(model2.predict(input))[0]
     return jsonify(preds)
 
-@app.route('/<string:arr>', methods=['GET'])
-def slash(arr):
-    return jsonify(arr)
+@app.route('/add_to_database/<string:arr>', methods=["GET"])
+def add_to_database(arr):
+    pass
+@app.route('/route_query/<string:query>/<string:token>', methods=["GET"])
+def route_query(query, token):
+    pass
+@app.route("/", methods=['GET'])
+def welcome():
+    help_= dict()
+    help_["Why and what ?"] = "This app is an aid to help farmers grow their crops and to curate a database for agriculture reasearchers from the sensors to study and apply machine learning on"
+    help_["Sensors"]= "Sensors would be community owned by farmers and they can get to know about their individual fields"
+    help_["Endpoints Available"]= ["/what2grow/<string:arr>", "/stats", '/pred_fertilizer/<string:arr>']
+    help_["How to Rest call an array ?"]= "arr at the end of the url expects array elements with a delimiter as a, didnt use json because easier to parse this ways"
+    help_["/what2grow/"]= "This inputs expected parameters from the farmer and tells him what to grow this season"
+    help_["/pred_fertilizer/"]= "This uses expected data from the farmer about the soil and tells the fertilizer to use"
+    help_["Example query"]= "https://kisan-app.herokuapp.com/1%2%3%4%5"
+    help_["add_to_database/<string:arr>"]= "Command to add values to the database"
+    help_["route_query/<string:query>/<string:token>"]= "Route queries to the open database for researchers who have access token"
+    return jsonify(help_)
+
+    
 
 if __name__ == '__main__':
     app.run(port=8081, debug=True)
